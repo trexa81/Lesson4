4dd50f1133004432219723096c6f052d4da39197
trexa81 <trex81@yandex.ru>
2021-11-12 13:20:33 +0300

    ДЗ_4.1+4.2

--- lesson4/Program.cs
+++ lesson4/Program.cs
@@ -10,6 +10,7 @@ namespace lesson4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите целые числа через пробел и нажмите: ввод");
             int Add(params int[] array)
             {
                 return array.Sum();
@@ -17,6 +18,60 @@ namespace lesson4
             var x = Console.ReadLine();
             var y = x.Split().Select(int.Parse).ToArray();
             Console.WriteLine(Add(y));
-        }  
+            Console.WriteLine("\t\t***\n");
+
+            (string firstName, string lastName, string patronymic)[] users = GetFullNames();
+            PrintUsers(users);
+        }
+        static (string firstName, string lastName, string patronymic) GetFullName()
+        {
+            Console.WriteLine("Привет! Как тебя зовут?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine($"Привет! " + firstName + " Какая у  тебя фомилия?");
+            string lastName = Console.ReadLine();
+            Console.WriteLine($"Привет! " + firstName + " Какое у  тебя отчество?");
+            string patronymic = Console.ReadLine();
+            return (firstName, lastName, patronymic);
+        }
+
+        static (string firstName, string lastName, string patronymic)[] GetFullNames()
+        {
+            Console.WriteLine("Введите количество пользователей:");
+            int count = Convert.ToInt32(Console.ReadLine());
+            (string firstName, string lastName, string patronymic)[] users =
+                new (string firstName, string lastName, string patronymic)[count];
+            for (int i = 0; i < users.Length; i++)
+            {
+                users[i] = GetFullName();
+                Console.WriteLine($"Пользователь {FormatUserData(users[i])} сохранен");
+            }
+            Console.WriteLine("Ввод данных завершён. Нажмите любую клавишу...");
+            Console.ReadKey();
+            Console.Clear();
+
+            return users;
+        }
+        static void PrintUser((string firstName, string lastName, string patronymic) user)
+        {
+            Console.WriteLine(FormatUserData(user));
+        }
+
+        static string FormatUserData((string firstName, string lastName, string patronymic) user)
+        {
+            return $"{user.firstName} {user.lastName} {user.patronymic}";
+        }
+        static void PrintUsers((string firstName, string lastName, string patronymic)[] users)
+        {
+            Console.WriteLine("Вывод всех пользователей:");
+            for (int i = 0; i < users.Length; i++)
+            {
+                PrintUser(users[i]);
+            }
+        }   
+
+
+
+
     }
-} 
+}
+ 
